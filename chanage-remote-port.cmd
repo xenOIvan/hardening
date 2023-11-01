fb2c11799c9fa805157172bc34ba8ebf47500fe4
xenOIvan <xenoivan@gmail.com>
2023-11-01 21:47:27 +0330

    Create chanage-remote-port.cmd

---
+++ chanage-remote-port.cmd
@@ -0,0 +1,15 @@
+# 👉 set new windows remote port
+$portvalue = 963
+
+# 👉 Get current remote port number
+Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "PortNumber"
+
+# 👉 change port number
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "PortNumber" -Value $portvalue 
+
+# 👉 add firewall rules
+New-NetFirewallRule -DisplayName 'RemoteDesktop-963-TCP-In' -Profile 'Public' -Direction Inbound -Action Allow -Protocol TCP -LocalPort $portvalue 
+New-NetFirewallRule -DisplayName 'RemoteDesktop-963-UDP-In' -Profile 'Public' -Direction Inbound -Action Allow -Protocol UDP -LocalPort $portvalue
+
+# 👉 show newly set port number
+Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "PortNumber"
